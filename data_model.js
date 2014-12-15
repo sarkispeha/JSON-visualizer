7ff84b47d197e951b06143489e897b3d12d3423a
Sarkis Peha <sarkispeha@gmail.com>
2014-12-15 15:12:51 -0700

    helper function now determines if passed object has keys within it, still trying to figure out how to automatically iterate through subkeys

--- data_model.js
+++
@@ -1,5 +0,0 @@
-// HTML content of template
-var templateSource = $('#startData').html();
-
-//compile source
-var templateFunc = Handlebars.compile(templateSource);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,20 +1,49 @@
-var keyArr = [];
-for(var i=0; i<Object.keys(data).length; i++) {
-	var keyName = Object.keys(data)[i];
-	var subKeyName = Object.keys(data[keyName])[i];
-	console.log(keyName);
-	console.log(subKeyName);
-	// console.log(data[keyName]._id);
-	for(var key in data){
-		keyArr.push(key);
-		console.log(keyArr);
+
+/*helper function to determine if more
+for in loops must be run*/	
+var hasMoreKeys = function(obj) {
+	    try {
+	    	if(Object.keys(obj));
+		}
+		catch(err) {
+	    	return false
+		}
+	if(Object.keys(obj).length === 0) return false;
+    return true;
+    // return obj.hasOwnProperty();
+    // && obj[key] === value;
 	}
-	for(var i = 0; i < keyArr.length; i++){
+var bigFunc = function(calledObj){
+	for(var i=0; i<Object.keys(calledObj).length; i++) {
+		var keyName = Object.keys(calledObj)[i];//iterating through first layer of keys, keyName is the i-th index of the returned array
+		// var subKeyName = Object.keys(data[keyName])[i];
+		console.log(keyName);
+		// console.log(subKeyName);
+		// console.log(data[keyName]._id);
 		var appendDIV = document.createElement('DIV');
-		var t = document.createTextNode(keyArr[i]);
-		console.log(t);
+		var t = document.createTextNode(keyName);
 		appendDIV.appendChild(t);
 		document.getElementById("myDiv").appendChild(appendDIV);
+		if (hasMoreKeys(data[keyName])) {
+			var keyArr = [];
+			for(var key in data[keyName]){
+				keyArr.push(key);
+				console.log(keyArr);
+			}
+		for(var i = 0; i < keyArr.length; i++){
+			var appendDIV = document.createElement('DIV');
+			var t = document.createTextNode(keyArr[i]);
+			console.log(t);
+			appendDIV.appendChild(t);
+			appendDIV.className = 'child';
+			document.getElementById("myDiv").appendChild(appendDIV);
+		}
+			
+		}	
 	}
+	// else{
+	// 	return false;
+	// }
+
 	
 }
